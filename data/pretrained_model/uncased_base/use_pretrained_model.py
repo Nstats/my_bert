f50b3941d068c2c85c2a8c940d56da89e4814274
Nstats <17801238715@163.com>
2018-12-25 18:11:06 +0800

    use pretrained model

--- data/pretrained_model/uncased_base/use_pretrained_model.py
+++ data/pretrained_model/uncased_base/use_pretrained_model.py
@@ -4,6 +4,8 @@ saver = tf.train.import_meta_graph('./bert_model.ckpt.meta')
 
 sess = tf.Session()
 saver.restore(sess, './bert_model.ckpt')
+
 for ele in tf.trainable_variables():
-    print(ele.name)
+    print(ele)
+
 sess.close()

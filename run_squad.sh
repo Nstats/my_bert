8288deea36e2ddf4078155d9571bc175a9cd45f7
Nstats <17801238715@163.com>
2019-01-05 16:49:07 +0800

    ...

--- run_squad.sh
+++ run_squad.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python run_squad.py
-python ./data/squad/evaluate-v2.0.py './data/squad/dev-v2.0.json' './data/squad/output/predictions.json'
+python ./data/squad/evaluate-v2.0.py './data/squad/dev-v2.0.json' './data/squad/output/predictions.json' --na-prob-file ./data/squad/output/null_odds.json
 
 # {
 #   "exact": 71.9952834161543,
@@ -12,4 +12,8 @@ python ./data/squad/evaluate-v2.0.py './data/squad/dev-v2.0.json' './data/squad/
 #   "NoAns_exact": 72.49789739276703,
 #   "NoAns_f1": 72.49789739276703,
 #   "NoAns_total": 5945
+#   "best_exact": 72.83753053145793,
+#   "best_exact_thresh": -5.559882640838623,
+#   "best_f1": 76.10335982008996,
+#   "best_f1_thresh": -4.95320200920105
 # }

c999a7ff48a9315a938d8b19b92679f21abe9cb5
Nstats <17801238715@163.com>
2019-03-29 17:17:32 +0800

    3.29

--- run_squad2_train.sh
+++ run_squad2_train.sh
@@ -10,7 +10,7 @@ python run_squad.py \
 --train_batch_size=16 \
 --learning_rate=5e-5 \
 --num_train_epochs=3.0 \
---max_seq_length=300 \
+--max_seq_length=290 \
 --doc_stride=128 \
 --output_dir=./data/squad/output_layer-1 \
 --version_2_with_negative=True \
